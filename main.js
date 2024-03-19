9c626e349ed59b480a6a39fca8fa076e9ba7c4dd
Evan Byrnes <ebyrnes1999@gmail.com>
2024-03-19 15:18:12 -0400

    add: hover effects and hex color text
    Add hover effects to scheme selector menu and button in top section and display hex color text associated with each color in the scheme. Add default black monochrome scheme with hex color text.

--- main.js
+++ main.js
@@ -8,27 +8,36 @@ const colorTwo = document.getElementById('color-two')
 const colorThree = document.getElementById('color-three')
 const colorFour = document.getElementById('color-four')
 const colorFive = document.getElementById('color-five')
+const colorOneText = document.getElementById('color-one-text')
+const colorTwoText = document.getElementById('color-two-text')
+const colorThreeText = document.getElementById('color-three-text')
+const colorFourText = document.getElementById('color-four-text')
+const colorFiveText = document.getElementById('color-five-text')
 let colorBoxHex;
 let schemeSelection;
 
 async function renderScheme(e){
     e.preventDefault()
     schemeSelection = colorSchemes.value;
-    console.log(colorBoxHex)
-    console.log(schemeSelection)
     const response = await fetch(`https://www.thecolorapi.com/scheme?hex=${colorBoxHex}&mode=${schemeSelection}&count=5`)
     const data = await response.json()
-    console.log(data)
     const color1 = data.colors[0].hex.value
     const color2 = data.colors[1].hex.value
     const color3 = data.colors[2].hex.value
     const color4 = data.colors[3].hex.value
     const color5 = data.colors[4].hex.value
+
     colorOne.style.backgroundColor = color1;
     colorTwo.style.backgroundColor = color2;
     colorThree.style.backgroundColor = color3;
     colorFour.style.backgroundColor = color4;
     colorFive.style.backgroundColor = color5;
+
+    colorOneText.textContent = color1;
+    colorTwoText.textContent = color2;
+    colorThreeText.textContent = color3;
+    colorFourText.textContent = color4;
+    colorFiveText.textContent = color5;
 }
 
 colorBox.addEventListener('input', () =>{
